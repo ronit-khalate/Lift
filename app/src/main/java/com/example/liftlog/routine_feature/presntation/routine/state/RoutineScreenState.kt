ced24a24e41401bff19455e289caf9c7197c66c8
ronit-khalate <ronitkhalate243@gmail.com>
2024-07-31 03:56:02 +0530

    added default values to all parameter of RoutredScreenState

--- app/src/main/java/com/example/liftlog/routine_feature/presntation/routine/state/RoutineScreenState.kt
+++ app/src/main/java/com/example/liftlog/routine_feature/presntation/routine/state/RoutineScreenState.kt
@@ -3,8 +3,8 @@ package com.example.liftlog.routine_feature.presntation.routine.state
 import com.example.liftlog.core.data.model.Exercise
 
 data class RoutineScreenState(
-    val routineId:String,
-    val routineName:String,
-    val note:String,
-    val exerciseList:List<Exercise>
+    val routineId:String?=null,
+    val routineName:String="",
+    val note:String="",
+    val exerciseList:List<Exercise> = emptyList()
 )
